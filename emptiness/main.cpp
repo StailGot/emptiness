2a9843d205a46f7ffd1a504030bdff4c27744f45
Alex <sanchesz@list.ru>
2016-02-13 00:30:27 +0300

    init

--- emptiness/main.cpp
+++ emptiness/main.cpp
@@ -1,4 +1,33 @@
+#include <SFML/Window.hpp>
+#include <iostream>
+
+#include <gl/glew.h>
+#pragma comment(lib,"opengl32.lib")
+
+
 int main( int argc, char * argv [] )
 {
+  sf::Window window { sf::VideoMode( 700, 700 ), "opengl window" };
+  ::glewInit();
+
+  auto render =
+    []
+  {
+    GLfloat color [] = { 0.3f, 0.f, 0.f, 0.f };
+    ::glClearBufferfv( GL_COLOR, 0, color );
+  };
+
+  while (window.isOpen())
+  {
+    sf::Event e {};
+    while (window.pollEvent( e ))
+    {
+      if (e.type == sf::Event::EventType::Closed)
+        window.close();
+      else
+        render(), window.display();
+    }
+  }
+
   return 0;
 }
