7d7d90005352f5f704fb2078492ffa9dd7559745
Alex <sanchesz@list.ru>
2016-02-13 00:32:07 +0300

    clean

--- emptiness/main.cpp
+++ emptiness/main.cpp
@@ -1,9 +1,8 @@
-#include <SFML/Window.hpp>
-#include <iostream>
-
 #include <gl/glew.h>
 #pragma comment(lib,"opengl32.lib")
+#include <SFML/Window.hpp>
 
+#include <iostream>
 
 int main( int argc, char * argv [] )
 {
